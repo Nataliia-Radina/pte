cb917884deaa237b1e0546bacc3cc5f5b626250a
PixelsCommander <denis.radin@gmail.com>
2014-05-27 22:54:21 +0200

    Slideshow added

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$('.main_image').slidesjs({
+    width: 949,
+    height: 449,
+    play: {
+        effect: "fade",
+        speed: 1000,
+        interval: 5000,
+        auto: true,
+        // [boolean] Start playing the slideshow on load.
+        pauseOnHover: false
+    },
+    navigation: {
+        active: false,
+        effect: "fade"
+    }
+});
