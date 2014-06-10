8249c8ca0113655b98915be4e7254532e21391ef
PixelsCommander <denis.radin@gmail.com>
2014-06-10 23:08:36 +0200

    Merge branch 'master' of https://github.com/Nataliia-Radina/pte
    Conflicts:
    	css/style.css
    	reconstruction.html

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
- $(function(){
+$(function () {
     $('.main_image').slidesjs({
         width: 949,
         height: 449,
@@ -15,21 +15,21 @@
             effect: "fade"
         },
         callback: {
-            loaded: function(){
+            loaded: function () {
                 // hide navigation and pagination
                 $('.slidesjs-navigation').hide(0);
             }
         }
     });
 
-    $(".slidesjs-slide").click(function(e){
+    $(".slidesjs-slide").click(function (e) {
         e.preventDefault();
         $('.slidesjs-next.slidesjs-navigation').trigger('click');
     });
 
     var on_top = $('.on_top')[0];
 
-    $(document).scroll(function(e) {
+    $(document).scroll(function (e) {
         if (document.body.scrollTop > 0 && on_top.style.display !== 'block') {
             on_top.style.display = 'block';
         } else if (document.body.scrollTop === 0 && on_top.style.display !== 'none') {
@@ -39,32 +39,43 @@
 
     $('.expander').simpleexpand();
 
-    $('.hide').click(function(e){
+    $('.hide').click(function (e) {
         this.parentNode.style.display = 'none';
         e.preventDefault();
     });
 
 
-     $('.map area').mouseover(function() {
-         showMapRegion($(this).attr('class').split('region-').pop());
-     });
-     $('.map area').mouseout(function() {
-         hideMapRegion($(this).attr('class').split('region-').pop());
-     });
-     $('.map-links a').mouseover(function() {
-         showMapRegion($(this).attr('class').split('region-').pop());
-     });
-     $('.map-links a').mouseout(function() {
-         hideMapRegion(parseInt($(this).attr('class').split('region-').pop()));
-     });
+    $('.map area').mouseover(function () {
+        showMapRegion($(this).attr('class').split('region-').pop());
+    });
+    $('.map area').mouseout(function () {
+        hideMapRegion($(this).attr('class').split('region-').pop());
+    });
+    $('.map-links a').mouseover(function () {
+        showMapRegion($(this).attr('class').split('region-').pop());
+    });
+    $('.map-links a').mouseout(function () {
+        hideMapRegion(parseInt($(this).attr('class').split('region-').pop()));
+    });
 
-     showMapRegion = function(num) {
-         $('.map img.region-'+num).show();
-         $('.map-links .region-'+num).addClass('active');
-     };
+    showMapRegion = function (num) {
+        $('.map img.region-' + num).show();
+        $('.map-links .region-' + num).addClass('active');
+    };
 
-     hideMapRegion = function(num) {
-         $('.map img.region-'+num).hide();
-         $('.map-links .region-'+num).removeClass('active');
-     };
+    hideMapRegion = function (num) {
+        $('.map img.region-' + num).hide();
+        $('.map-links .region-' + num).removeClass('active');
+    };
+
+    $('.ceo_link').click(function (e) {
+        e.preventDefault();
+        $('.ceo_intro').hide();
+        $('.ceo_block').hide();
+        var showBlockId = this.id + '_block';
+        $('#' + showBlockId).fadeIn(500);
+        $('html, body').animate({
+            scrollTop: $(".heading.position").offset().top
+        }, 500);
+    });
 });
